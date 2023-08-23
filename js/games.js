c1f95a4abd7bbeca04b2573f411348ba49108ec2
Alexandr Kotenko <kpshmt@gmail.com>
2023-08-23 16:04:38 +0300

    update

--- js/games.js
+++
@@ -1,6 +0,0 @@
-function srcModal6() {
-    document.getElementById('gameFrame6').src = 'https://demogamesfree.pragmaticplay.net/gs2c/openGame.do?lang=en&cur=USD&websiteUrl=https://slotcatalog.com/&gcpif=2273&gameSymbol=vs20kraken2&jurisdiction=99&lobbyUrl=https://slotcatalog.com/';
-    }
-    
-   
-    
